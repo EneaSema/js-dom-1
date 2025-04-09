309a8775fde5d3f6287156bf707b2fce3c6c48d9
EneaSema <sema.enea1993@gmail.com>
2025-04-10 01:57:24 +0200

    work-on-bonus-no-resolve

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-console.log("ciao");
+// console.log("ciao");
 
 const light = document.getElementById("lamp");
 console.log(light);
@@ -6,7 +6,31 @@ console.log(light);
 const buttonCommander = document.getElementById("btn-spento");
 console.log(buttonCommander);
 
+let lightOn = false;
+console.log(lightOn);
+
 buttonCommander.addEventListener(`click`, function () {
   light.src = "./img/yellow_lamp.png";
   buttonCommander.innerHTML = "Spegni";
+  lightOn = true;
+  console.log(lightOn);
+  return lightOn;
 });
+
+if (lightOn === true) {
+  buttonCommander.addEventListener(`click`, function () {
+    light.src = "./img/white_lamp.png";
+    buttonCommander.innerHTML = "Spegni";
+    lightOn = false;
+    console.log(lightOn);
+    return lightOn;
+  });
+} else if (lightOn !== true) {
+  buttonCommander.addEventListener(`click`, function () {
+    light.src = "./img/yellow_lamp.png";
+    buttonCommander.innerHTML = "Spegni";
+    lightOn = true;
+    console.log(lightOn);
+    return lightOn;
+  });
+}
