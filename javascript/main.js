90c6f435008b3913da355dd28fae1bf3aa21ad77
EneaSema <sema.enea1993@gmail.com>
2025-04-09 18:22:34 +0200

    solution-exercise-base

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
 console.log("ciao");
+
+const light = document.getElementById("lamp");
+console.log(light);
+
+const buttonCommander = document.getElementById("btn-spento");
+console.log(buttonCommander);
+
+buttonCommander.addEventListener(`click`, function () {
+  light.src = "./img/yellow_lamp.png";
+  buttonCommander.innerHTML = "Spegni";
+});
